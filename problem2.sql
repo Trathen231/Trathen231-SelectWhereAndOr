177b03e00b80352c4702e50f3ea074aca595877c
Trathen231 M <trathen231@gmail.com>
2023-07-17 16:47:45 +0000

    SQL Select WHERE and OR

---
+++ problem2.sql
@@ -0,0 +1,3 @@
+SELECT id, first_name, last_name, salary
+FROM employee
+WHERE salary > 100000.00 OR salary < 50000.00;
---
+++ problem3.sql
@@ -0,0 +1,3 @@
+SELECT id, first_name, last_name, salary
+FROM employee
+WHERE salary > 50000.00 AND first_name <> 'Steve';
